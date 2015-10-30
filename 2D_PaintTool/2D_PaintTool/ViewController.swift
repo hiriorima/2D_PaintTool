26b9304cf7c725b53c974970d5c53e3f1f65a8db
会津慎弥 <b1013043@fun.ac.jp>
2015-10-30 16:54:09 +0900

    First Commit

---
+++ 2D_PaintTool/2D_PaintTool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2D_PaintTool
+//
+//  Created by 会津慎弥 on 2015/10/30.
+//  Copyright © 2015年 会津慎弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
