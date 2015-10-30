c8059414700e965c9b345c1880b99f63c5ed141d
蛯名真紀 <b1013107@fun.ac.jp>
2015-10-30 20:35:40 +0900

    iPad横に設定変更

---
+++ 2D_PaintTool/2D_PaintTool/View2Controller.swift
@@ -0,0 +1,35 @@
+//
+//  View2Controller.swift
+//  2D_PaintTool
+//
+//  Created by 蛯名真紀 on 2015/10/30.
+//  Copyright © 2015年 会津慎弥. All rights reserved.
+//
+
+import UIKit
+
+class View2Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- 2D_PaintTool/2D_PaintTool/ViewController.swift
+++ 2D_PaintTool/2D_PaintTool/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text="たて椅子"
     }
 
     override func didReceiveMemoryWarning() {
