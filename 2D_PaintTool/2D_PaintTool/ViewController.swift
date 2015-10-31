0d9761ce585b82f7708526924e66ff6704b58586
会津慎弥 <b1013043@fun.ac.jp>
2015-11-01 03:37:32 +0900

    マージミス修正

--- 2D_PaintTool/2D_PaintTool/ViewController.swift
+++ 2D_PaintTool/2D_PaintTool/ViewController.swift
@@ -16,11 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-<<<<<<< HEAD
-       
-=======
         
->>>>>>> 3716e1de7bee5f570be8f135435ed812dafb97a3
     }
 
     override func didReceiveMemoryWarning() {
